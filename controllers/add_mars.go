dcc0550804b29a9c4ee0e37b98aafa80ccc3fe48
qijun <qijun1994@hotmail.com>
2020-10-06 09:30:42 +0800

    clean code

--- controllers/add_mars.go
+++ controllers/add_mars.go
@@ -1,12 +1,9 @@
 /*
 Copyright 2020 The Alibaba Authors.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,9 +16,9 @@ package controllers
 import (
 	"github.com/alibaba/kubedl/api/marsjob/v1alpha1"
 	controllers "github.com/alibaba/kubedl/controllers/mars"
-	controllerruntime "sigs.k8s.io/controller-runtime"
 
 	"github.com/alibaba/kubedl/pkg/job_controller"
+	"sigs.k8s.io/controller-runtime"
 )
 
 func init() {
