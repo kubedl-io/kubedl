c06d2746de57c414da4e447aa00df30ee11b525a
Zhongpu Diao <zpdiao@gmail.com>
2021-06-15 17:49:07 +0800

    feat(co-sched): add unit test

--- pkg/gang_schedule/coscheduler/scheduler_test.go
+++ pkg/gang_schedule/coscheduler/scheduler_test.go
@@ -16,35 +16,42 @@ var (
 )
 
 func TestCreateGang(t *testing.T) {
-	workerNumber := 3
+	testCases := []int32{
+		1, 2, 3, 9,
+	}
 
-	testManager, _ := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{})
-	testScheduler := NewKubeCoscheduler(testManager)
-	testJob := testutilv1.NewTestJob(workerNumber)
+	for workerNumber := range testCases {
+		testManager, _ := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{})
+		testScheduler := NewKubeCoscheduler(testManager)
+		testJob := testutilv1.NewTestJob(workerNumber)
 
-	testObject, _ := testScheduler.CreateGang(testJob, testJob.Spec.TestReplicaSpecs)
-	testPodGroup := testObject.(*v1alpha1.PodGroup)
-	assert.Equal(t, testPodGroup.Name, testutilv1.TestJobName)
-	assert.Equal(t, testPodGroup.Namespace, metav1.NamespaceDefault)
-	assert.Equal(t, testPodGroup.Spec.MinMember, int32(workerNumber))
+		testObject, _ := testScheduler.CreateGang(testJob, testJob.Spec.TestReplicaSpecs)
+		testPodGroup := testObject.(*v1alpha1.PodGroup)
+		assert.Equal(t, testPodGroup.Name, testutilv1.TestJobName)
+		assert.Equal(t, testPodGroup.Namespace, metav1.NamespaceDefault)
+		assert.Equal(t, testPodGroup.Spec.MinMember, int32(workerNumber))
+	}
 }
 
 func TestBindPodToGang(t *testing.T) {
-	workerNumber := int(3)
+	testCases := []int32{
+		1, 2, 3, 9,
+	}
 
-	testManager, _ := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{})
-	testScheduler := NewKubeCoscheduler(testManager)
-	testJob := testutilv1.NewTestJob(workerNumber)
+	for workerNumber := range testCases {
+		testManager, _ := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{})
+		testScheduler := NewKubeCoscheduler(testManager)
+		testJob := testutilv1.NewTestJob(workerNumber)
 
-	testPodSpec := testutilv1.NewTestReplicaSpecTemplate()
-	testPodSpec.Labels = make(map[string]string)
-	testObject, _ := testScheduler.CreateGang(testJob, testJob.Spec.TestReplicaSpecs)
-	testPodGroup := testObject.(*v1alpha1.PodGroup)
+		testPodSpec := testutilv1.NewTestReplicaSpecTemplate()
+		testPodSpec.Labels = make(map[string]string)
+		testObject, _ := testScheduler.CreateGang(testJob, testJob.Spec.TestReplicaSpecs)
+		testPodGroup := testObject.(*v1alpha1.PodGroup)
 
-	testScheduler.BindPodToGang(&testPodSpec, testPodGroup)
-	assert.Equal(t, testPodSpec.Labels["pod-group.scheduling.sigs.k8s.io"], testPodGroup.Name)
+		testScheduler.BindPodToGang(&testPodSpec, testPodGroup)
+		assert.Equal(t, testPodSpec.Labels["pod-group.scheduling.sigs.k8s.io"], testPodGroup.Name)
 
-	assert.Equal(t, testPodSpec.Labels["pod-group.scheduling.sigs.k8s.io/name"], testPodGroup.Name)
-	assert.Equal(t, testPodSpec.Labels["pod-group.scheduling.sigs.k8s.io/min-available"], string(testPodGroup.Spec.MinMember))
-
+		assert.Equal(t, testPodSpec.Labels["pod-group.scheduling.sigs.k8s.io/name"], testPodGroup.Name)
+		assert.Equal(t, testPodSpec.Labels["pod-group.scheduling.sigs.k8s.io/min-available"], string(testPodGroup.Spec.MinMember))
+	}
 }
