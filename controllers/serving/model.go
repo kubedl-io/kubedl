fab49634729904cd8129bda0b4692fcb3bff0a4f
jian-he <jianhe788@gmail.com>
2021-07-27 00:33:42 -0700

    fix

--- controllers/serving/model.go
+++ controllers/serving/model.go
@@ -27,7 +27,7 @@ func (ir *InferenceReconciler) buildModelLoaderInitContainer(mv *v1alpha1.ModelV
 	c.VolumeMounts = append(c.VolumeMounts, v1.VolumeMount{Name: sharedVolumeName, MountPath: destModelPath})
 	// Model loader is an init container to move model artifacts from source path to destination.
 	//c.Command = []string{"/bin/sh -c \" cp -r " + v1alpha1.DefaultModelPathInImage + "/* " + destModelPath + "\""}
-	c.Command = []string{"sleep 5000"}
+	c.Command = []string{"/bin/sleep 5000"}
 
 	return c, nil
 }
