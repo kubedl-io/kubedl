fa18c4808cbdeabe74cb08686ef6a48ca755c75e
jian-he <jianhe788@gmail.com>
2021-07-29 16:16:41 -0700

    test

---
+++ scripts/run_tf_test_job.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+echo "#### Running TF test job"
+kubectl apply -f example/tf/tf_job_mnist_distributed_simple.yaml
+
+for ((i=1;i<20;i++));
+do
+  set +e
+  PODS=$(kubectl get pod | grep "tf-distributed-simple-worker" | grep "Completed"  | wc -l)
+  set -e
+  if [ "$PODS" -eq 3 ]; then
+    echo "Succeed: TF distributed test job."
+    exit 0
+  fi
+  echo "waiting for job to complete"
+  sleep 5
+done
+
+echo "Failed: TF distributed test job."
+exit 1
