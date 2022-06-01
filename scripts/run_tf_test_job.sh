940a7f91f6b30b207a4e3a6984465644b8f7a20f
SimonCqk <cqk0100@gmail.com>
2022-06-01 16:20:36 +0800

    test: add elastic scale unit tests.
    Signed-off-by: SimonCqk <cqk0100@gmail.com>

--- scripts/run_tf_test_job.sh
+++ scripts/run_tf_test_job.sh
@@ -4,10 +4,11 @@ set -e
 echo "#### Running TF test job"
 kubectl apply -f example/tf/tf_job_mnist_distributed_simple.yaml
 
-for ((i=1;i<20;i++));
+for ((i=1;i<40;i++));
 do
   set +e
   PODS=$(kubectl get pod | grep "tf-distributed-simple-worker" | grep "Completed"  | wc -l)
+  kubectl logs tf-distributed-simple-worker-0
   set -e
   if [ "$PODS" -eq 3 ]; then
     echo "Succeed: TF distributed test job."
